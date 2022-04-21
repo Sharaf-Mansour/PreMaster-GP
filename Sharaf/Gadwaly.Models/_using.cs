1699fce9208d8a2da601a92a2dd56e033132ada8
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-04-21 16:30:00 +0200

    Create _using.cs

---
+++ Sharaf/Gadwaly.Models/_using.cs
@@ -0,0 +1,28 @@
+﻿global using FluentValidation;
+global using FluentValidation.Results;
+global using Gadwaly.Models.Validation;
+namespace Gadwaly.Models;
+public static class ValidationExtensions
+{
+    public static ValidationResult Validate<T>(this T? type) => type switch
+    {
+        string => new DataValidator().Validate(type as string),
+        IDList => new IDListValidator().Validate(type as IDList),
+        LookUp => new LookUpValidator().Validate(type as LookUp),
+        StudentArchive => new StudentArchiveValidator().Validate(type as StudentArchive),
+        null => new ValidationResult
+        {
+            Errors =
+    {
+        new("error", $"Value is null ")
+    }
+        },
+        _ => new ValidationResult
+        {
+            Errors =
+            {
+                new ("error",$"type of {type?.GetType().ToString() ?? "Null Type"} ValidationResult is not yet implemented")
+            }
+        }
+    };
+}
