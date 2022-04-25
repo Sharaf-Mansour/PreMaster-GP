667f1354bb648fc3020e9e8c4516b44c5502b6f4
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-04-25 12:27:13 +0200

    Setup API

--- Sharaf/Gadwaly.API/Program.cs
+++ Sharaf/Gadwaly.API/Program.cs
@@ -1,43 +1,39 @@
+using System.ComponentModel.Design.Serialization;
+using Gadwaly.Models;
+using Gadwaly.Service.StudentCreationService;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwagger().UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
+
+
+
+
+
+
+
+
+
+
+app.MapPost("/CreateStudent", async (StudentArchive Student) =>
+(BoolString)(await new StudentRegistrationService().CreateStudentAsync(Student)));
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+record BoolString(bool Action, string Msg)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public static explicit operator BoolString((bool, string) x) => new(x.Item1, x.Item2);
+    public static implicit operator (bool, string)(BoolString x) => x;
 }
+
