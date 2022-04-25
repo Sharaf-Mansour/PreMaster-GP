55378e0325d79a011424c11abfd7f43c95971c39
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-04-25 12:29:49 +0200

    Update StudentRegistrationService.cs

--- Sharaf/Gadwaly.Service/StudentCreationService/StudentRegistrationService.cs
+++ Sharaf/Gadwaly.Service/StudentCreationService/StudentRegistrationService.cs
@@ -1,11 +1,42 @@
 ﻿using Gadwaly.Broker.Brokers;
 using Gadwaly.Models;
-
-namespace Gadwaly.Service.StudentCreationService
+namespace Gadwaly.Service.StudentCreationService;
+public class StudentRegistrationService
 {
-    public class StudentRegistrationService
+    public async ValueTask<(bool, string)> CreateStudentAsync(StudentArchive Student)
     {
-        //   public async ValueTask CreatStudentAsync(StudentArchive Student)
-
+        var Msg = "Error: Student Not Valid";
+        var IsAdded = false;
+        var Validation = Student.Validate();
+        if (Validation.IsValid)
+        {
+            Msg = "Error: Student Already Exist";
+            bool IsNewStudent = await StudentRegistrationBroker.GetStudentAsync(Student.StudentID) is null;
+            if (IsNewStudent)
+            {
+                Msg = "Error In Database";
+                IsAdded = await StudentRegistrationBroker.CreateStudentAsync(Student);
+                if (IsAdded)
+                    Msg = "Success";
+            }
+        }
+        return (IsAdded, Msg);
+    }
+    public async ValueTask<(bool, string)> ApproveStudentsAsync(IDList[]? Students)
+    {
+        var IsApproved = false;
+        var Msg = "Error: Parameter is null";
+        if (Students is not null)
+        {
+            Msg = "Error: List has no students";
+            if (Students.Length > 0)
+            {
+                (IsApproved, var count) = await StudentAccountBroker.AcceptStudentListAsync(Students);
+                Msg = $" Warning  {count} Students of {Students.Length} has been Updated ";
+                if (IsApproved)
+                    Msg = $" Success  {count} Students of {Students.Length} has been Updated ";
+            }
+        }
+        return (IsApproved, Msg);
     }
 }
