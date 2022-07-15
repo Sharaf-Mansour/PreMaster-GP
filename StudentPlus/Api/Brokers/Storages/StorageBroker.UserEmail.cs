cc2d37d9b2f9d002a128162441092691af8f2d7b
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-07-15 02:13:44 +0200

    fix model

--- StudentPlus/Api/Brokers/Storages/StorageBroker.UserEmail.cs
+++ StudentPlus/Api/Brokers/Storages/StorageBroker.UserEmail.cs
@@ -5,5 +5,4 @@ public partial class StorageBroker : IStorageBroker
     public async ValueTask<bool> SelectUserAccountByEmailAsync(string Email) => await Connection.QuerySingleOrDefaultAsync<bool>(sql: "SelectUserAccountByEmail", param: new { Email }, commandType: CommandType.StoredProcedure);
     public async ValueTask<(bool IsLoggedin, int ID)> SelectUserAccountByEmailAndPasswordAsync(string Email, string Password) => await Connection.QuerySingleOrDefaultAsync<(bool IsLoggedin, int ID)>(sql: "SelectUserAccountByEmailAndPassword", param: new { Email, Password }, commandType: CommandType.StoredProcedure);
     public async ValueTask<int> InsertStudentAccountAsync(StudentAccount Model) => await Connection.ExecuteAsync(sql: "InsertStudentAccount", param: new { Model.ApplyYear, Model.Email, Model.Password, Model.Phone }, commandType: CommandType.StoredProcedure);
-
 }
