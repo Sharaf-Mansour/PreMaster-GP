7c84c906f931b03c8c5aef33525a561c002936b6
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-07-26 03:56:59 +0200

    CodeRub

--- StudentPlus/Data/NavigationService.cs
+++ StudentPlus/Data/NavigationService.cs
@@ -2,7 +2,8 @@ namespace StudentPlus.Data;
 public class NavigationService
 {
     public string? Page { get; set; }
-    public bool StayLoggedin { get; set; } = true;
+    public StudentAccount Model { get; set; }
+    public bool StayLoggedin { get; set; }
     public bool IsLoggedIn { get; set; } = false;
     public AccountType AccountType { get; set; } = AccountType.Student;
     public event Action? OnChange;
