cc90abe5ea2afac872512321ce8445cbb6600618
Sharaf-Mansour <sharaf_1015@yahoo.com>
2022-07-27 18:30:48 +0200

    Add full student data

--- StudentPlus/Models/StudentAccount.cs
+++ StudentPlus/Models/StudentAccount.cs
@@ -2,6 +2,28 @@
 public partial class StudentAccount : AccountLogin
 {
     public string? Phone { get; set; }
+    public string? ProfilePicture { get; set; }
+    public string? FullNameEN { get; set; }
+    public string? FullNameArabic { get; set; }
+    public bool? Gender { get; set; }
+    public int? Country { get; set; }
+    public string? City { get; set; }
+    public string? Address { get; set; }
+    public string? ZipCode { get; set; }
+    public int? Nationality { get; set; }
+    public string? NID { get; set; }
+    public string? Passport { get; set; }
+    public DateTime? Birthday { get; set; }
+    public string? PassportImg { get; set; }
+    public string? NIDImage { get; set; }
+    public string? BirthCImg { get; set; }
+
+    public string? BCImg { get; set; }
+    public string? DCImg { get; set; }
+    public string? MCImg { get; set; }
+    public string? PhdCimg { get; set; }
+    public string? LastQual { get; set; }
+
     public string? ConfirmPassword { get; set; }
     public bool PrivacyCheck { get; set; }
     public async ValueTask<bool> IsValidAsync() => (await new StudentAccountValidator().ValidateAsync(this)).IsValid;
